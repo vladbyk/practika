66b938440de94c307431d4326954656da7669183
vladbyk <byk_vlad@inbox.ru>
2023-01-12 13:36:12 +0300

    add absolute route

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -3,18 +3,22 @@ import {Link, NavLink} from "react-router-dom";
 import logo from './logo.png'
 
 function Header() {
-  const exitHundler = () =>{
-    sessionStorage.clear()
-    window.location.assign('/login')
-  }
-  return (
-    <nav>
-        <p><NavLink to='/'><img src={logo} alt='logo'/></NavLink></p>
-        <p><NavLink to='/rates'>Курсы валют</NavLink></p>
-        <p><NavLink to='/countries'>Страны</NavLink></p>
-        <p onClick={exitHundler}>Выход</p>
-    </nav>
-  );
+    const exitHundler = () => {
+        sessionStorage.clear()
+        window.location.assign('/login')
+    }
+    return (
+        <nav>
+            <div>
+                <p><NavLink to='/'><img src={logo} alt='logo'/></NavLink></p>
+            </div>
+            <div>
+                <p><NavLink to='/rates'>Курсы валют</NavLink></p>
+                <p><NavLink to='/countries'>Страны</NavLink></p>
+                <p onClick={exitHundler}>Выход</p>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
