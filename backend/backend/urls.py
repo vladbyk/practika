cbf523fae6aceb2da8d1aca383ff417d0d1a9047
vladbyk <byk_vlad@inbox.ru>
2023-01-12 14:31:35 +0300

    check docs path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('api/', include('parser.urls')),
     path('api/', include('djoser.urls.jwt')),
     path('openapi-schema/', get_schema_view(
-        info={'swagger':'2.0'},
         title="Covid info",
         description="statistics of information for 2020 according to coronavirus cases and currency exchange rate "
                     "changes",
